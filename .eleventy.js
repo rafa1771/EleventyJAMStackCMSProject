4d7feeeca39fbcb6572948a349b9cc9cc96fc18f
Vizt Nivlir <raf1771@gmail.com>
2025-03-03 17:07:31 +0100

    Second commit

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/style.css");
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
